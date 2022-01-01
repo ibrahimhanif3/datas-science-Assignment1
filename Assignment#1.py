b113d368cc02c909626451677973f9f306838472
ibrahimhanif3 <you@example.com>
2022-01-01 23:20:05 +0500

    first commit

---
+++ Assignment#1.py
@@ -0,0 +1,26 @@
+#Assignment1 
+# Ibrahim Hanif
+#Question-1
+poem = "Twinkle, twinkle, little star, \n\t How I wonder what you are! \n\t\t Up above the world so high, \n\t\t Like a diamond in the sky. \n winkle, twinkle, little star, \n\t How I wonder what you are!"
+print (poem)
+
+#Question-2
+from platform import python_version
+print (python_version())
+
+#Question-3
+from datetime import datetime
+now = datetime.now()
+print("now =", now)
+
+#Question-4
+fname = input("Input your First Name : ")
+lname = input("Input your Last Name : ")
+print ("Name  " + lname + " " + fname)
+
+#Question-5
+a = int(input("enter first number: "))
+b = int(input("enter second number: "))
+sum = a + b
+ 
+print("Total:", sum)
